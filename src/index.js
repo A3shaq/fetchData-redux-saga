d6896b5c72e7df8999d555358bd66614b0332918
Muhammad Arshaq <arshaqshakeel3@gmail.com>
2021-03-17 02:03:15 +0500

    * Unsplash images

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import App from './App';
 import Main from "./main"
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
